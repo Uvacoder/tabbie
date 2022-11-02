df543aef11c98634f162794717a79021011f1dea
uvacoder <uva22940coder@gmail.com>
2022-11-02 17:44:20 -0400

    Update SearchHeading.js

--- src/components/SearchHeading.js
+++ src/components/SearchHeading.js
@@ -3,14 +3,11 @@ import React from "react";
 export default function SearchHeading(props) {
   return (
     <div>
-      <p className="text-3xl font-mono text-center">
-        Tabbie
-        <sup>
-          <small className="text-base">α</small>
-        </sup>
+      <p className="text-5xl font-mono text-center">
+        Tabbie : Startpage        
       </p>
       <p className="text-base italic text-center ">
-        {props.name} the World.
+        {props.name} search.
       </p>
     </div>
   );
